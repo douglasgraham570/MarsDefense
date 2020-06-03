37840b0aea1525566794625a34b12fabb6b5693c
Douglas Graham <dig2@rice.edu>
2020-06-03 10:07:04 -0700

    Enemy movement
    Corrected path for enemy movement

---
+++ Mars Defense/Assets/Scripts/Enemy.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    //enemy movement speed along path
+	public float speed = 1;
+
+	public static Transform[] waypoints;
+    public float waypointRadius = 0.1f;
+
+    //current waypoint enemy is heading towards
+    private Transform currentWaypoint;
+
+    private int waypointIndex = 0;
+
+    Rigidbody2D rgb;
+
+    //Start is called before first frame
+    void Start() {
+
+        waypoints = Waypoints.waypoints;
+
+        //set initial waypoint 
+        currentWaypoint = waypoints[waypointIndex];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get a direction vector for enemy movement
+        Vector3 direction = currentWaypoint.position - transform.position;
+
+        //if enemy reaches current waypoint, go to next one
+        if (direction.magnitude <= waypointRadius)
+        {
+            waypointIndex += 1;
+
+            //destroy if we're at last waypoint
+            if (waypointIndex >= waypoints.Length - 1)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            currentWaypoint = waypoints[waypointIndex];
+            direction = currentWaypoint.position - transform.position;
+        }
+
+        //move the enemy along the normalized movement vector
+        Vector3 translation = direction.normalized;
+
+        transform.Translate(translation* Time.deltaTime * speed);
+    }
+}
---
+++ Mars Defense/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+	public static Transform[] waypoints;
+    public Transform waypointParent;
+
+    // Awake is called immediately upon loading
+    void Awake()
+    {
+        //get reference to waypoints game object
+        waypoints = new Transform[waypointParent.childCount];
+
+
+        //loop through the waypoints and put them into the array
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = waypointParent.GetChild(i);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
