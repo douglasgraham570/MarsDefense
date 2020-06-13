543e337dbb764ac4446b286f5fbd97e444adaabe
Douglas Graham <dig2@rice.edu>
2020-06-12 21:20:29 -0700

    Tower Selection UI
    Made basic UI for selecting and placing towers

---
+++ Mars Defense/Assets/Scripts/TowerStore.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TowerStore : MonoBehaviour
+{
+    public GameObject tower1PrebuildPrefab;
+    public RectTransform buttonTransform;
+
+    //instantiate tower prebuild at the button's transform
+    public void InstantiatePrebuild()
+    {
+        GameObject temp = new GameObject();
+
+        Transform tempTransform = temp.transform;
+
+        tempTransform.position = buttonTransform.position;
+
+        Debug.Log("Instantiating tower");
+        Instantiate(tower1PrebuildPrefab, tempTransform);
+        return;
+    }
+}
