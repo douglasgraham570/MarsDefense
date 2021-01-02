51858f35b246c7deef992527007ef5eeeca8a50e
Douglas Graham <dig2@rice.edu>
2021-01-02 09:36:09 -0800

    end game
    end game tag pops up when player loses

--- Mars Defense/Assets/Scripts/EndGame.cs
+++ Mars Defense/Assets/Scripts/EndGame.cs
@@ -6,12 +6,15 @@ using UnityEngine;
 public class EndGame : MonoBehaviour
 {
     private Lives health;
+    private GameObject gameOver;
 
     // Start is called before the first frame update
     void Start()
     {
         health = GetComponent<Lives>();
         Debug.Log("Got a reference to the game over canvas ");
+
+        gameOver = GameObject.FindGameObjectWithTag("GameOverPanel");
     }
 
     // Update is called once per frame
@@ -24,8 +27,9 @@ public class EndGame : MonoBehaviour
     {
         if (health.lives <= 0)
         {
-            //game over panel shows up
-       
+            //game over canvas is visible now
+            Canvas gameOverCanvas = gameOver.GetComponent<Canvas>();
+            gameOverCanvas.enabled = true;
 
             //freezes time
             Time.timeScale = 0;
