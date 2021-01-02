3452a1846613e67629754a71ff1a40cfe2a37b27
Douglas Graham <dig2@rice.edu>
2021-01-02 09:42:43 -0800

    start menu pops up
    start menu is enabled now

--- Mars Defense/Assets/Scripts/startTheGame.cs
+++ Mars Defense/Assets/Scripts/startTheGame.cs
@@ -5,8 +5,20 @@ using UnityEngine;
 public class startTheGame : MonoBehaviour
 {
 
+    private GameObject startGameMenu;
+
+    private void Awake()
+    {
+        startGameMenu = GameObject.FindGameObjectWithTag("GameStartPanel");
+        Canvas gameStartCanvas = startGameMenu.GetComponent<Canvas>();
+        gameStartCanvas.enabled = true;
+    }
+
     public void startGame()
     {
         Debug.Log("Starting the game");
+
+        //make the start game menu visible
+
     }
 }
