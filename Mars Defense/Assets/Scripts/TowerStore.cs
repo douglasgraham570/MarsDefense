23163bc63aa79b479aecd6f5f762db7457f12650
Douglas Graham <dig2@rice.edu>
2021-01-02 17:48:17 -0800

    fixed building before game begins
    as title

--- Mars Defense/Assets/Scripts/TowerStore.cs
+++ Mars Defense/Assets/Scripts/TowerStore.cs
@@ -9,7 +9,8 @@ public class TowerStore : MonoBehaviour
     public int cost = 5;
     public RectTransform buttonTransform;
     private int towerNumber = 1;
-    
+
+    public bool hasGameStarted = false;
 
     Currency currency;
 
@@ -19,27 +20,39 @@ public class TowerStore : MonoBehaviour
         currency = manager.GetComponent<Currency>();
     }
 
+    public void TellStoreGameStarted()
+    {
+        hasGameStarted = true;
+    }
+
     //instantiate tower prebuild at the button's transform if
     //you can afford it
     public void InstantiatePrebuild()
     {
-
-
-        if (currency.money >= cost) 
+        if (hasGameStarted)
         {
-            GameObject temp = new GameObject();
+            if (currency.money >= cost)
+            {
+                GameObject temp = new GameObject();
 
-            Transform tempTransform = temp.transform;
+                Transform tempTransform = temp.transform;
 
-            tempTransform.position = buttonTransform.position;
+                tempTransform.position = buttonTransform.position;
 
-            //Debug.Log("Instantiating tower");
-            Instantiate(tower1PrebuildPrefab, tempTransform);
-            return;
-     
-        } else
-        {
-            Debug.Log("Not enough money to buy");
+                //Debug.Log("Instantiating tower");
+                Instantiate(tower1PrebuildPrefab, tempTransform);
+                return;
+
+            }
+            else
+            {
+                Debug.Log("Not enough money to buy");
+            }
         }
+        else
+        {
+            Debug.Log("Cannot buy. Game hasnot started yet!");
+        }
+
     }
 }
